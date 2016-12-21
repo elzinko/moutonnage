bd7726807a1c63fd0a4101504ac642ccdae3daca
Thomas Couderc <thomas.couderc@gmail.com>
2016-12-21 23:16:23 +0100

    add swagger

---
+++ src/main/java/fr/couderc/moutonnage/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2001-2016 Group JCDecaux. 
+ * 17 rue Soyer, 92523 Neuilly Cedex, France.
+ * All rights reserved. 
+ * 
+ * This software is the confidential and proprietary information
+ * of Group JCDecaux ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you
+ * entered into with Group JCDecaux.
+ */
+
+package fr.couderc.moutonnage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {                                    
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
+}
